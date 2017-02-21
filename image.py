b116239f27828b7f2a55c8257fbce69b233f4c04
Nathan Petrangelo <nathan@petrangelo.org>
2017-02-21 12:00:11 -0500

    Use 2 cameras

--- image.py
+++ image.py
@@ -8,15 +8,20 @@ import math
 FOVTangent = math.tan(math.radians(26))
 focalLength = 320/FOVTangent
 
-def initCamera():
+def initCameras():
 	os.system("v4l2-ctl -c exposure_auto=1 -c exposure_absolute=5")
 	global capture
+	global capture1
 	capture = cv2.VideoCapture(0)
-	capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640); 
-	capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480);
+	capture1 = cv2.VideoCapture(1)
+	capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640) 
+	capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+	capture1.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+	capture1.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 capture = None
-initCamera()
+capture1 = None
+initCameras()
 res, image = capture.read()
 res, image = capture.read()
 res, image = capture.read()
@@ -32,3 +37,5 @@ for contour in p.filter_contours_output:
 	print(angle)
 print(p.filter_contours_output)
 cv2.imwrite('image-contours.jpg', image)
+res, image = capture1.read()
+cv2.imwrite('image-capture1.jpg', image)
