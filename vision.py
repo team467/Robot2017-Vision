14945ba03826a05f583c52d51c639c623f699bae
Nathan Petrangelo <nathan@petrangelo.org>
2017-02-21 20:29:00 -0500

    Changed Averaging Method

--- vision.py
+++ vision.py
@@ -37,26 +37,51 @@ last_time = time.time()*1000
 print("Starting Loop")
 while True:
 	time.sleep(1.0/30)
-	gyro = gyroTable.getNumber("Y-Axis Angle") # reading at image (before latency)
+	#gyro = gyroTable.getNumber("Y-Axis Angle") # reading at image (before latency)
+	gyro = table.getNumber("gyro")
 	res, image = capture.read()
 	p.process(image)
 	contours = p.filter_contours_output
 	table.putBoolean("seeTwo", len(contours) == 2)
-	if len(contours) == 2:
-		x1,y1,w1,h1 = cv2.boundingRect(contours[0])
-		x2,y2,w2,h2 = cv2.boundingRect(contours[1])
-		x = statistics.mean([x1,x2])
-		y = statistics.mean([y1,y2])
-		w = statistics.mean([w1,w2])
-		h = statistics.mean([h1,h2])
-		center = x + w/2
-# 		angle = (center - 640)*26/640
+	table.putNumber("numContours", len(contours))
+# 	xSum, ySum, wSum, hSum = [0, 0, 0, 0]
+	xSum = 0
+	ySum = 0
+	wSum = 0
+	hSum = 0
+	for contour in contours:
+		x, y, w, h = cv2.boundingRect(contour)
+		xSum = xSum + x
+		ySum = ySum + y
+		wSum = wSum + w
+		hSum = hSum + h
+	if (len(contours) > 0):
+		xAvg = xSum / len(contours)
+		yAvg = ySum / len(contours)
+		wAvg = wSum / len(contours)
+		hAvg = hSum / len(contours)
+		center = xAvg + wAvg/2
 		angle = math.degrees(math.atan((center - 320) / focalLength))
-		table.putNumber("x", x)
-		table.putNumber("y", y)
-		table.putNumber("w", w)
-		table.putNumber("h", h)
+		table.putNumber("x", xAvg)
+		table.putNumber("y", yAvg)
+		table.putNumber("w", wAvg)
+		table.putNumber("h", hAvg)
 		table.putNumber("angle", gyro + angle)
+# 	if len(contours) == 2:
+# 		x1,y1,w1,h1 = cv2.boundingRect(contours[0])
+# 		x2,y2,w2,h2 = cv2.boundingRect(contours[1])
+# 		x = statistics.mean([x1,x2])
+# 		y = statistics.mean([y1,y2])
+# 		w = statistics.mean([w1,w2])
+# 		h = statistics.mean([h1,h2])
+# 		center = x + w/2
+# # 		angle = (center - 640)*26/640
+# 		angle = math.degrees(math.atan((center - 320) / focalLength))
+# 		table.putNumber("x", x)
+# 		table.putNumber("y", y)
+# 		table.putNumber("w", w)
+# 		table.putNumber("h", h)
+# 		table.putNumber("angle", gyro + angle)
 	#print("Number of contours: {}".format(len(contours)))
 # 	f.write('STEP: ' + str(p.find_contours_output) + '\n')
 	now = time.time()*1000
